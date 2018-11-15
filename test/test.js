7540f6dc39d88299aaa1a62201a0073398c50cbf
gherardovarando <gherardo.varand@gmail.com>
2018-11-15 09:10:24 +0100

    [upd] some more test

--- test/test.js
+++ test/test.js
@@ -27,7 +27,7 @@ describe('Graph', function() {
           ]).graph['c']),
         ['b']);
     });
-  });//end constructor
+  }); //end constructor
 
   describe('#addNode()', function() {
     it('should add node to empty graph', function() {
@@ -44,6 +44,23 @@ describe('Graph', function() {
       assert.deepEqual((new Graph(['a'])).addNode('b'), new Graph(['a', 'b']));
       assert.deepEqual((new Graph([])).addNode('a'), new Graph(['a']));
     });
-  });//end addNode
+  }); //end addNode
 
+  describe('#removeNode()', function() {
+    it('should remove a node', function() {
+      assert.deepEqual((new Graph(['a', 'b', 'c'])).removeNode('a').nodes(), ['b', 'c']);
+      assert.deepEqual((new Graph(['a', 'b', 'c'])).removeNode('b').nodes(), ['a', 'c']);
+      assert.deepEqual((new Graph(['a','b','c'])).removeNode('c').nodes(),['a', 'b']);
+    });
+    it('should return the graph if the node is not there', function() {
+      const g = new Graph(['a', 'b', 'c'], [['a','c'],['c','b']]);
+      const g2 = new Graph(['a', 'b', 'c'], [['a','c'],['c','b']]);
+      assert.deepEqual(g.removeNode('xxx'), g2);
+      assert.deepEqual(g.removeNode(1), g2);
+    });
+    it('should remove also the respective edges', function() {
+       const g = new Graph(['a', 'b', 'c'], [['a','c'],['c','b']]);
+       assert.deepEqual(g.removeNode('a').graph['c'], ['b']);
+    });
+  }); //end removeNode
 });
